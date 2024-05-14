094633e5f077d7a4774fccf684864a526536240a
Yana Fil <yanafill2070@gmail.com>
2024-05-14 15:48:47 +0300

    hw21

---
+++ HW21(Pilow)/multithreaded.py
@@ -0,0 +1,50 @@
+import threading
+from PIL import Image
+import os
+from urllib.request import urlopen
+from time import perf_counter
+
+def save_image(url, filename, height=200):
+    try:
+        image = Image.open(urlopen(url))
+        width = int(image.width * height / image.height)
+        image = image.resize((width, height))
+        image.save(filename + ".jpg")
+        print(f"Картинку завантажено!")
+    except Exception as e:
+        print(f"Помилка завантаження картинки з {url}: {e}")
+
+img_urls = [
+    'https://images.unsplash.com/photo-1516117172878-fd2c41f4a759',
+    'https://images.unsplash.com/photo-1532009324734-20a7a5813719',
+    'https://images.unsplash.com/photo-1524429656589-6633a470097c',
+    'https://images.unsplash.com/photo-1530224264768-7ff8c1789d79',
+    'https://images.unsplash.com/photo-1564135624576-c5c88640f235',
+    'https://images.unsplash.com/photo-1541698444083-023c97d3f4b6',
+    'https://images.unsplash.com/photo-1522364723953-452d3431c267',
+    'https://images.unsplash.com/photo-1513938709626-033611b8cc03',
+    'https://images.unsplash.com/photo-1507143550189-fed454f93097',
+    'https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e',
+    'https://images.unsplash.com/photo-1504198453319-5ce911bafcde',
+    'https://images.unsplash.com/photo-1530122037265-a5f1f91d3b99',
+    'https://images.unsplash.com/photo-1516972810927-80185027ca84',
+    'https://images.unsplash.com/photo-1550439062-609e1531270e',
+    'https://images.unsplash.com/photo-1549692520-acc6669e2f0c'
+]
+
+if not os.path.exists('img'):
+    os.makedirs('img')
+
+start_time = perf_counter()
+threads = []
+for url in img_urls:
+    filename = os.path.join('img', os.path.basename(url))
+    thread = threading.Thread(target=save_image, args=(url, filename))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+end_time = perf_counter()
+print(f"Час виконання програми (багатопотоковий): {end_time - start_time: 0.4f}")
