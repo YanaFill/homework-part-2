4d136736577e07b24002b124cf76c5395d815df6
Yana Fil <yanafill2070@gmail.com>
2024-09-25 18:45:42 +0300

    exam_final_version

--- exam/project_sb/accounts/views.py
+++ exam/project_sb/accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from .forms import UserProfileForm
+
 
 def register(request):
     if request.method == 'POST':
@@ -16,10 +18,19 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
 
+
 # Відображення профілю для залогованих користувачів
 @login_required
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    user_profile = request.user.userprofile
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserProfileForm(instance=user_profile)
+    return render(request, 'accounts/profile.html', {'form': form})
 
 
 # Вихід з акаунту
--- exam/project_sb/subject/admin.py
+++ exam/project_sb/subject/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Subject, LessonType
 
-admin.site.register(Subject)
 admin.site.register(LessonType)
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'teacher', 'lesson_type', 'control', 'specialty', 'semester')
+    search_fields = ('name', 'teacher', 'specialty')
+    list_filter = ('lesson_type', 'control', 'semester')
+    ordering = ('name',)
